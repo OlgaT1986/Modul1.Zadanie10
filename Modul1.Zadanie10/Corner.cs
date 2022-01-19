595f4a8fda7c85206ee888caf19d7f84043cd179
Admin <Admin@ADMIN->
2022-01-19 15:07:05 +0700

    Добавьте файлы проекта.

---
+++ Modul1.Zadanie10/Corner.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul1.Zadanie10
+{
+    class Corner
+    {
+        // переменные внутри класса, закрытые
+        private double gradus;  
+        private double min;      
+        private double sec;
+        // ствойства, открытые
+        public double Gradus   
+        {
+            // действие при получении свойства, возврат в значение поля
+            get 
+            {
+                return gradus; 
+            }
+            // действие при установки свойства
+            set 
+            {
+                gradus = value % 360;
+            }
+        }
+        public double Min
+        {
+            get
+            {
+                return min;
+            }
+            set
+            {
+                // в градусы накапливаем если больше 60 мин, а затем остаток от деления - в минуты
+                Gradus += value / 60; 
+                min = value % 60;  
+            }
+        }
+        public double Sec
+        {
+            get
+            {
+                return sec;
+            }
+            set
+            {
+               // накапливаем в свойства Min, остаток от деления - в секунды
+                Min += value / 60; 
+                sec = value % 60;
+            }
+        }
+        // конструктор с типом double, открытый. Помещены значения мер угла
+        public Corner(double gradus, double min, double sec) 
+        {
+            this.Gradus = gradus;  
+            this.Min = min;        
+            this.Sec = sec;        
+        }
+        // метод перевода мер угла градусы в радианы
+        public double ToRadians()  
+        {
+            // возврат значения в радианы
+            return (gradus + (min + sec / 60) / 60) * Math.PI / 180;  
+        }
+    }
+}
+
---
+++ Modul1.Zadanie10/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul1.Zadanie10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ввод мер угла-градусы, минуты, секунды
+            Console.Write("Введите меру угла - градусы:");
+            double gradus_ = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите меру угла - минуты:");
+            double min_ = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите меру угла - секунды:");
+            double sec_ = Convert.ToInt32(Console.ReadLine());
+            // создаю экземпляр класса-объект
+            Corner corner = new Corner(gradus_, min_, sec_);    
+            // преобразую введенные данные угла в радианы
+            double totalRadians = corner.ToRadians();
+            Console.WriteLine(totalRadians);
+            Console.Write("для завершения нажмите любую клавишу");
+            Console.ReadKey();
+        }
+    }
+
+}
+
